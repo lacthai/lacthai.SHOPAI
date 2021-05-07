db9180fc63da79347a8006578f8212701caa4816
Thái Gia Lạc <75162324+lacthai@users.noreply.github.com>
2021-05-07 20:14:52 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,221 @@
+// đăng ký , đăng nhập
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("b");
+var modal3 = document.getElementById("c");
+var modal4 = document.getElementById("d");
+var click = document.getElementById("sign_up");
+var click2 = document.getElementById("Log_in");
+var click3 = document.getElementById("Log-in_inmodal");
+var click4 = document.getElementById("Sign-up_inmodal");
+var click5 = document.getElementById("btn-next-to-login");
+click.onclick = function() {
+  modal.style.display = "block";
+  modal2.style.display = "block";
+  modal3.style.display = "block";
+  modal4.style.display = "none";
+}
+click2.onclick = function() {
+  modal.style.display = "block";
+  modal2.style.display = "block";
+  modal3.style.display = "none";
+  modal4.style.display = "block";
+}
+click3.onclick = function() {
+  modal.style.display = "block";
+  modal2.style.display = "block";
+  modal3.style.display = "none";
+  modal4.style.display = "block";
+}
+click4.onclick = function() {
+  modal.style.display = "block";
+  modal2.style.display = "block";
+  modal3.style.display = "block";
+  modal4.style.display = "none";
+}
+click5.onclick = function() {
+  modal.style.display = "block";
+  modal2.style.display = "block";
+  modal3.style.display = "none";
+  modal4.style.display = "block";
+}
+var span1 = document.getElementsByClassName("close1")[0];
+span1.onclick = function() {
+  modal.style.display = "none";
+  modal2.style.display = "none";
+  modal3.style.display = "none";
+}
+var span2 = document.getElementsByClassName("close2")[0];
+span2.onclick = function() {
+  modal.style.display = "none";
+  modal2.style.display = "none";
+  modal3.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
+    modal4.style.display = "none";
+  }
+}
+var btnback = document.getElementsByClassName("btn-back1")[0];
+btnback.onclick = function() {
+  modal.style.display = "none";
+  modal2.style.display = "none";
+  modal3.style.display = "none";
+}
+var btnback2 = document.getElementsByClassName("btn-back2")[0];
+btnback2.onclick = function() {
+  modal.style.display = "none";
+  modal2.style.display = "none";
+  modal3.style.display = "none";
+}
+//báo lỗi log in, log out
+
+//bot chat
+function FunctionBotChat() {
+  var x = document.getElementById("bot_chat");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// xóa cart
+var remove_cart = document.getElementsByClassName("btn-danger");
+for (var i = 0; i < remove_cart.length; i++) {
+  var button = remove_cart[i]
+  button.addEventListener("click", function () {
+    var button_remove = event.target
+    button_remove.parentElement.parentElement.remove();
+    updatecart();
+  })
+}
+
+// update cart 
+function updatecart() {
+    var cart_item = document.getElementsByClassName("cart-items")[0];
+    var cart_rows = cart_item.getElementsByClassName("cart-row");
+    var total = 0;
+    var total2 = 0;
+    var number_product_add = 0;
+    for (var i = 0; i < cart_rows.length; i++) {
+      var cart_row = cart_rows[i]
+      var price_item = cart_row.getElementsByClassName("cart-price")[0]
+      var quantity_item = cart_row.getElementsByClassName("cart-quantity-input")[0]
+      var price = parseFloat(price_item.innerText)// chuyển một chuổi string sang number để tính tổng tiền.
+      var quantity = quantity_item.value // lấy giá trị trong thẻ input
+      total = total + (price * quantity)
+      number_product_add = cart_rows.length;
+    }
+    document.getElementsByClassName("cart-total-price")[0].innerText = total + '$'
+    document.getElementsByClassName("header_cart-notice")[0].innerText = number_product_add
+    // Thay đổi text = total trong .cart-total-price. Chỉ có một .cart-total-price nên mình sử dụng [0].
+  }
+// thay đổi số lượng sản phẩm
+var quantity_input = document.getElementsByClassName("cart-quantity-input");
+for (var i = 0; i < quantity_input.length; i++) {
+  var input = quantity_input[i];
+  input.addEventListener("change", function (event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+      input.value = 1;
+    }
+    updatecart()
+  })
+}
+
+// Thêm vào giỏ
+var add_cart = document.getElementsByClassName("btn-cart");
+for (var i = 0; i < add_cart.length; i++) {
+  var add = add_cart[i];
+  add.addEventListener("click", function (event) {
+
+    var button = event.target;
+    var product = button.parentElement.parentElement;
+    var img = product.parentElement.getElementsByClassName("img-prd")[0].src
+    var title = product.getElementsByClassName("content-product-h3")[0].innerText
+    var price = product.getElementsByClassName("price")[0].innerText
+    addItemToCart(title, price, img)
+    // Khi thêm sản phẩm vào giỏ hàng thì sẽ hiển thị modal
+    modal5.style.display = "block";
+    number_product_add = cart_rows.length;
+    updatecart()
+    document.getElementsByClassName("header_cart-notice")[0].innerText = number_product_add
+  })
+}
+
+// Thêm vào giỏ
+
+var add_cart = document.getElementsByClassName("btn-cart");
+for (var i = 0; i < add_cart.length; i++) {
+  var add = add_cart[i];
+  add.addEventListener("click", function (event) {
+
+    var button = event.target;
+    var product = button.parentElement.parentElement;
+    var img = product.parentElement.getElementsByClassName("img_pr")[0].src
+    var title = product.getElementsByClassName("home-product-item_name")[0].innerText
+    var price = product.getElementsByClassName("home-product-item_price-current")[0].innerText
+    addItemToCart(title, price, img)
+    // Khi thêm sản phẩm vào giỏ hàng thì sẽ hiển thị modal
+    modal5.style.display = "block";
+    number_product_add = cart_rows.length;
+    updatecart()
+  })
+}
+
+
+function addItemToCart(title, price, img) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cart_title = cartItems.getElementsByClassName('cart-item-title')
+    // Nếu title của sản phẩm bằng với title mà bạn thêm vao giỏ hàng thì sẽ thông cho user.
+    for (var i = 0; i < cart_title.length; i++) {
+      if (cart_title[i].innerText == title) {
+        alert('Products already in the cart')
+        return
+      }
+    }
+  
+    var cartRowContents = `
+    <div class="cart-item cart-column">
+        <img class="cart-item-image" src="${img}" width="100" height="100">
+        <span class="cart-item-title">${title}</span>
+    </div>
+    <span class="cart-price cart-column">${price}</span>
+    <div class="cart-quantity cart-column">
+        <input class="cart-quantity-input" type="number" value="1">
+        <button class="btn btn-danger" type="button">X</button>
+    </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', function () {
+      var button_remove = event.target
+      button_remove.parentElement.parentElement.remove()
+      updatecart()
+    })
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', function (event) {
+      var input = event.target
+      if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+      }
+      updatecart()
+    })
+  }
+
+
+
+  //product
+
+  $(function () {
+    $(selector).click(function (e) { 
+      e.preventDefault();
+      alert()
+    });
+  });
+  
+  
+  
